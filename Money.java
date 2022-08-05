171d87947c71c3ac17dd8090cfea73557ca26038
gabriel-ramos44 <gabriel.santosramos@live.com>
2022-08-05 01:07:57 -0300

    Commit 4

--- Money.java
+++ Money.java
@@ -1,5 +1,7 @@
 abstract class Money  {
   protected int amount;
+  protected String currency;
+   
   public boolean equals(Object object)  {
     Money money = (Money) object;
     return this.amount == money.amount && getClass().equals(money.getClass());
@@ -8,28 +10,39 @@ abstract class Money  {
   abstract Money times(int multiplier);
 
   static Money dollar(int amount){
-    return new Dollar(amount);
+    return new Dollar(amount, "USD");
   }
 
   static Money franc(int amount){
-    return new Franc(amount);
+    return new Franc(amount, "CHF");
+  }
+
+  Money(int amount, String currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  String currency() {
+    return this.currency;
   }
 }
 
 class Dollar extends Money {
-  Dollar(int amount) {
-    this.amount = amount;  
+  Dollar(int amount, String currency) {
+    super(amount, currency);
   }
 
   Money times(int multiplier) {
-    return new Dollar(this.amount * multiplier);
+    return Money.dollar(this.amount * multiplier);
   }
+}
 
-@@ -23,7 +33,7 @@ class Franc extends Money {
-    this.amount = amount;
+class Franc extends Money {
+  Franc(int amount, String currency) {
+    super(amount, currency);
   }
 
   Money times(int multiplier)  {
-    return new Franc(this.amount * multiplier);
+    return Money.franc(this.amount * multiplier);
   }
 }
