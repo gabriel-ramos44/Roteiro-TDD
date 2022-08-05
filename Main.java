9b23a0e8ddf22ab5d8a79abeee349ac62fbb424c
gabriel-ramos44 <gabriel.santosramos@live.com>
2022-08-05 01:09:19 -0300

    Commit 5 + fix 4

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
-Testes tests = new Testes();
-tests.testEquality();
-tests.testMultiplication();
-tests.testFrancMultiplication();
+public class Main {
+  public static void main(String[] args){
+    Testes tests = new Testes();
+    tests.testEquality();
+    tests.testMultiplication();
+    tests.testFrancMultiplication();
+    tests.testCurrency();
+  }
 }
-}
-
